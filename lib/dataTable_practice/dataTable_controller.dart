c508a5712ce7398082cf4afca01da7718e67b2d8
Vivek <tailorvivekgec19@gmail.com>
2023-06-20 11:37:38 +0530

    dataTable

---
+++ lib/dataTable_practice/dataTable_controller.dart
@@ -0,0 +1,39 @@
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class DataTableController extends GetxController
+{
+
+  List<Map> database = [
+    {"id":01,"name":"Ajay","course":"Flutter","duration":12},
+    {"id":02,"name":"Jay","course":"Python","duration":16},
+    {"id":03,"name":"Jaydeep","course":"Animation","duration":8},
+    {"id":05,"name":"Deep","course":"Web Design","duration":4},
+    {"id":16,"name":"Dipesh","course":"Cyber Security","duration":24},
+    {"id":01,"name":"Ajay","course":"Flutter","duration":12},
+    {"id":02,"name":"Jay","course":"Python","duration":16},
+    {"id":03,"name":"Jaydeep","course":"Animation","duration":8},
+    {"id":05,"name":"Deep","course":"Web Design","duration":4},
+    {"id":16,"name":"Dipesh","course":"Cyber Security","duration":24},
+    {"id":01,"name":"Ajay","course":"Flutter","duration":12},
+    {"id":02,"name":"Jay","course":"Python","duration":16},
+    {"id":03,"name":"Jaydeep","course":"Animation","duration":8},
+    {"id":05,"name":"Deep","course":"Web Design","duration":4},
+    {"id":16,"name":"Dipesh","course":"Cyber Security","duration":24},
+    {"id":01,"name":"Ajay","course":"Flutter","duration":12},
+    {"id":02,"name":"Jay","course":"Python","duration":16},
+    {"id":03,"name":"Jaydeep","course":"Animation","duration":8},
+    {"id":05,"name":"Deep","course":"Web Design","duration":4},
+    {"id":16,"name":"Dipesh","course":"Cyber Security","duration":24},
+    {"id":01,"name":"Ajay","course":"Flutter","duration":12},
+    {"id":02,"name":"Jay","course":"Python","duration":16},
+    {"id":03,"name":"Jaydeep","course":"Animation","duration":8},
+    {"id":05,"name":"Deep","course":"Web Design","duration":4},
+    {"id":16,"name":"Dipesh","course":"Cyber Security","duration":24},
+    {"id":01,"name":"Ajay","course":"Flutter","duration":12},
+    {"id":02,"name":"Jay","course":"Python","duration":16},
+    {"id":03,"name":"Jaydeep","course":"Animation","duration":8},
+    {"id":05,"name":"Deep","course":"Web Design","duration":4},
+    {"id":16,"name":"Dipesh","course":"Cyber Security","duration":24},
+  ];
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:practice_getx/counter_app/counter_homeScreen.dart';
+import 'package:practice_getx/dataTable_practice/dataTable_practice.dart';
 import 'package:practice_getx/location/location_screen.dart';
 import 'package:practice_getx/runtime_permission/runtime_homeScreen.dart';
 import 'package:practice_getx/runtime_permission/splashScreen.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(
-    GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: "/location",
-      routes: {
-        "/":(p0) => CounterScreen(),
-        "/runtime":(p0) => RunTime_Splash(),
-        "/permission":(p0) =>Permission_Screen(),
-        "/location":(p0) => Location_Screen(),
+    Sizer(
+      builder: (context, orientation, deviceType) => GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: "/dataTable",
+        routes: {
+          "/":(p0) => CounterScreen(),
+          "/runtime":(p0) => RunTime_Splash(),
+          "/permission":(p0) =>Permission_Screen(),
+          "/location":(p0) => Location_Screen(),
+          "/dataTable":(p0) => DataTablePractice()
 
 
-      },
+        },
+      ),
     )
   );
 }
