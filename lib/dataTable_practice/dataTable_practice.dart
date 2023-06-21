a61216cf36b47353739ae899c5eafbb33a3a747f
Vivek <tailorvivekgec19@gmail.com>
2023-06-21 09:54:17 +0530

    dataTable

---
+++ lib/dataTable_practice/dataTable_practice.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:practice_getx/dataTable_practice/dataTable_controller.dart';
+import 'package:sizer/sizer.dart';
+
+class DataTablePractice extends StatefulWidget {
+  const DataTablePractice({super.key});
+
+  @override
+  State<DataTablePractice> createState() => _DataTablePracticeState();
+}
+
+class _DataTablePracticeState extends State<DataTablePractice> {
+
+  DataTableController control = Get.put(DataTableController());
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Text(""),
+          backgroundColor: Colors.teal,
+          centerTitle: true,
+          title: Text("DataTable",
+              style: TextStyle(fontWeight: FontWeight.w500,fontSize: 18.sp,color: Colors.white),),),
+        
+        body: ScrollConfiguration(
+          behavior: MyBehavior(),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+
+            child: Container(width: 600,
+              child: Padding(
+                padding:  EdgeInsets.all(8.0),
+
+                child: ListView(
+                  physics: BouncingScrollPhysics(),
+                  scrollDirection: Axis.vertical,
+                  children: [DataTable(
+                    border: TableBorder.all(width: 0.4,color: Colors.black38),
+                      headingRowColor: MaterialStateColor.resolveWith((states) => Colors.black),
+                      headingTextStyle: TextStyle(color: Colors.white,fontSize: 12.sp,fontWeight: FontWeight.w400),
+                      columns: [
+                        DataColumn(label: Text("No.")),
+                        DataColumn(label: Text("Name")),
+                        DataColumn(label: Text("Course")),
+                        DataColumn(label: Text("Duration")),
+
+                      ],
+                      rows: control.database.map((e) => DataRow(
+                      cells: [
+                        DataCell(Text("${e['id']}")),
+                        DataCell(Text("${e['name']}")),
+                        DataCell(Text("${e['course']}")),
+                        DataCell(Text("${e['duration']} months"),),
+                      ],
+                      ),).toList()
+                  )],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
+}
+
